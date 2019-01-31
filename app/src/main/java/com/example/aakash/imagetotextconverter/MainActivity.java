8c3fc34e6517600918df61e2c89147c47b7b6d4b
ALIAKBERAAKASH <ali852609@gmail.com>
2019-01-31 22:53:22 +0600

    Pick image from gallery and send it to TextActivity

--- app/src/main/java/com/example/aakash/imagetotextconverter/MainActivity.java
+++ app/src/main/java/com/example/aakash/imagetotextconverter/MainActivity.java
@@ -1,19 +1,31 @@
 package com.example.aakash.imagetotextconverter;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
     private  static final String TAG = MainActivity.class.toString();
+    private final int SELECT_PHOTO = 1;
 
     ImageView imageView;
+    FloatingActionButton imagePicker;
+    Bitmap receivedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +33,84 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imageView = findViewById(R.id.image_container);
+        imagePicker = findViewById(R.id.gallery);
+
+        imagePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //start image picker intent
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch(requestCode) {
+            case SELECT_PHOTO:
+                if(resultCode == RESULT_OK){
+                    try {
+                        final Uri imageUri = imageReturnedIntent.getData();
+                        final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                        final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                        imageView.setImageBitmap(selectedImage);
+                        receivedImage=selectedImage;
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Log.d(TAG, "onActivityResult: Failed to load image");
+                        Toast.makeText(this, "Failed to load Image", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        }
+
+
     }
 
     // When next button is clicked
     public void onNextClick(View view) {
 
         //start TextActivity
-        Intent intent = new Intent(MainActivity.this, TextActivity.class);
-        startActivity(intent);
+
+
+        if(receivedImage!=null)
+        {
+            /*Intent intent = new Intent(this, TextActivity.class);
+            Bundle b = new Bundle();
+            b.putParcelable("receivedImage", receivedImage);
+            intent.putExtras(b);
+            startActivity(intent);*/
+
+            //solution found from StackOverFlow
+            try {
+                //Write file
+                String filename = "bitmap.png";
+                FileOutputStream stream = this.openFileOutput(filename, Context.MODE_PRIVATE);
+                receivedImage.compress(Bitmap.CompressFormat.PNG, 100, stream);
+
+                //Cleanup
+                stream.close();
+                //receivedImage.recycle();
+
+                //Pop intent
+                Intent in1 = new Intent(this, TextActivity.class);
+                in1.putExtra("image", filename);
+                startActivity(in1);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+        else
+        {
+            Log.d(TAG, "onNextClick: recievedImage is empty!");
+            Toast.makeText(this, "No image was  selected!", Toast.LENGTH_SHORT).show();
+        }
+
+
 
     }
 
